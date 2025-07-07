803abdccd1e55ca067b7843510cc5d6f2996b998
zxl <wangrui.superman@gmail.com>
2025-07-07 16:44:22 +0800

    Update go@1.21.rb

--- Formula/go@1.21.rb
+++ Formula/go@1.21.rb
@@ -1,41 +1,32 @@
 class GoAT121 < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
   homepage "https://go.dev/"
-  url "https://go.dev/dl/go1.21.8.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.21.8.src.tar.gz"
-  sha256 "dc806cf75a87e1414b5b4c3dcb9dd3e9cc98f4cfccec42b7af617d5a658a3c43"
+  url "https://go.dev/dl/go1.21.13.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.21.13.src.tar.gz"
+  sha256 "71fb31606a1de48d129d591e8717a63e0c5565ffba09a24ea9f899a13214c34d"
   license "BSD-3-Clause"
+  revision 1
 
-  livecheck do
-    url "https://go.dev/dl/?mode=json"
-    regex(/^go[._-]?v?(1\.21(?:\.\d+)*)[._-]src\.t.+$/i)
-    strategy :json do |json, regex|
-      json.map do |release|
-        next if release["stable"] != true
-        next if release["files"].none? { |file| file["filename"].match?(regex) }
-
-        release["version"][/(\d+(?:\.\d+)+)/, 1]
-      end
-    end
-  end
+  no_autobump! because: :requires_manual_review
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "086fd0c47f5886d1e6375b7491b9741f703cc86140f5f2d22414ac6d46115781"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4899ec04a358067ca6c7c0fce6090df55bbc755c29b0e7d05db7ee5f54664c22"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6ddcfa2dc14942a3a7aafa2b22def3799b4b2a1d7cf3e6f5c82195afc6cd83d7"
-    sha256 cellar: :any_skip_relocation, sonoma:         "802883c4a6a7569eb09ad19c27ed449b091766a1738bd12597a810ae2ef72014"
-    sha256 cellar: :any_skip_relocation, ventura:        "696e8ee947d4ddd035827f8a4c607a09877cb683414f98211a476ebebc20c129"
-    sha256 cellar: :any_skip_relocation, monterey:       "cdc6761821cb4ed47b1fe6f74042af3250495eb6ee00871fedc9d5516442bd6c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d2ed8bbd49ba88b66e5150fbe47b55244a212d39b2550f25a44d9ed2f73384d7"
+    sha256 arm64_sequoia: "0653847518101d80daf1da2b2e14a2c6fd76c5821c6692ab8b0a492bfdb1761d"
+    sha256 arm64_sonoma:  "67a54a23b293d32ac840196dd114014e6d4b816861aa5a8422a0b1d1850fe7f9"
+    sha256 arm64_ventura: "baa064afbd10120172949e2632e9495537936a063bb73cbcee7b8136ca03441a"
+    sha256 sonoma:        "2e6c282c3dde6d05a2aad83939b28232521afa4530c2dd102e4092bd3faf32f0"
+    sha256 ventura:       "938a31dbe05636643409366c61afc7a6ba5d80abe3f591f8bb8846656e02e764"
+    sha256 x86_64_linux:  "c492b2a277c6abd6ab43cc429277882b5fb23f7195cb3f8aafdcfe41d6dad134"
   end
 
   keg_only :versioned_formula
 
+  # EOL with Go 1.23 release (2024-08-13)
+  # Ref: https://go.dev/doc/devel/release#policy
+  deprecate! date: "2024-08-16", because: :unsupported
+
   depends_on "go" => :build
 
   def install
-    ENV["GOROOT_BOOTSTRAP"] = buildpath/"gobootstrap"
-
     cd "src" do
       ENV["GOROOT_FINAL"] = libexec
       # Set portable defaults for CC/CXX to be used by cgo
@@ -49,13 +40,13 @@ class GoAT121 < Formula
 
     # Remove useless files.
     # Breaks patchelf because folder contains weird debug/test files
-    (libexec/"src/debug/elf/testdata").rmtree
+    rm_r(libexec/"src/debug/elf/testdata")
     # Binaries built for an incompatible architecture
-    (libexec/"src/runtime/pprof/testdata").rmtree
+    rm_r(libexec/"src/runtime/pprof/testdata")
   end
 
   test do
-    (testpath/"hello.go").write <<~EOS
+    (testpath/"hello.go").write <<~GO
       package main
 
       import "fmt"
@@ -63,7 +54,7 @@ class GoAT121 < Formula
       func main() {
           fmt.Println("Hello World")
       }
-    EOS
+    GO
 
     # Run go fmt check for no errors then run the program.
     # This is a a bare minimum of go working as it uses fmt, build, and run.
@@ -74,7 +65,7 @@ class GoAT121 < Formula
       system bin/"go", "build", "hello.go"
     end
 
-    (testpath/"hello_cgo.go").write <<~EOS
+    (testpath/"hello_cgo.go").write <<~GO
       package main
 
       /*
@@ -87,7 +78,7 @@ class GoAT121 < Formula
       func main() {
           C.hello()
       }
-    EOS
+    GO
 
     # Try running a sample using cgo without CC or CXX set to ensure that the
     # toolchain's default choice of compilers work
