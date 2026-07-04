d67c9e5271aca2f435094e7568278ce4d759b2b6
darthminion <darthminion@163.com>
2026-07-04 16:20:28 +0800

    Revert "Update go@1.12.rb"
    This reverts commit 3dc9dc755f74ca61ec04dc137f8caef0951de547.

--- Formula/go@1.12.rb
+++ Formula/go@1.12.rb
@@ -1,51 +1,67 @@
-class GoAT121 < Formula
+class GoAT112 < Formula
   desc "Open source programming language to build simple/reliable/efficient software"
-  homepage "https://go.dev/"
-  url "https://go.dev/dl/go1.21.13.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.21.13.src.tar.gz"
-  sha256 "71fb31606a1de48d129d591e8717a63e0c5565ffba09a24ea9f899a13214c34d"
-  license "BSD-3-Clause"
-  revision 1
+  homepage "https://golang.org"
 
-  bottle do
-    sha256 arm64_sequoia: "0653847518101d80daf1da2b2e14a2c6fd76c5821c6692ab8b0a492bfdb1761d"
-    sha256 arm64_sonoma:  "67a54a23b293d32ac840196dd114014e6d4b816861aa5a8422a0b1d1850fe7f9"
-    sha256 arm64_ventura: "baa064afbd10120172949e2632e9495537936a063bb73cbcee7b8136ca03441a"
-    sha256 sonoma:        "2e6c282c3dde6d05a2aad83939b28232521afa4530c2dd102e4092bd3faf32f0"
-    sha256 ventura:       "938a31dbe05636643409366c61afc7a6ba5d80abe3f591f8bb8846656e02e764"
-    sha256 x86_64_linux:  "c492b2a277c6abd6ab43cc429277882b5fb23f7195cb3f8aafdcfe41d6dad134"
+  stable do
+    url "https://dl.google.com/go/go1.12.9.src.tar.gz"
+    mirror "https://fossies.org/linux/misc/go1.12.9.src.tar.gz"
+    sha256 "ab0e56ed9c4732a653ed22e232652709afbf573e710f56a07f7fdeca578d62fc"
+
+    go_version = version.to_s.split(".")[0..1].join(".")
+    resource "gotools" do
+      url "https://go.googlesource.com/tools.git",
+          :branch => "release-branch.go#{go_version}"
+    end
   end
 
-  keg_only :versioned_formula
+  head do
+    url "https://go.googlesource.com/go.git"
 
-  # EOL with Go 1.23 release (2024-08-13)
-  # Ref: https://go.dev/doc/devel/release#policy
-  deprecate! date: "2024-08-16", because: :unsupported
-  disable! date: "2025-08-16", because: :unsupported
+    resource "gotools" do
+      url "https://go.googlesource.com/tools.git"
+    end
+  end
 
-  depends_on "go" => :build
+  # depends_on :macos => :yosemite
+
+  # Don't update this unless this version cannot bootstrap the new version.
+  resource "gobootstrap" do
+    url "https://storage.googleapis.com/golang/go1.7.darwin-amd64.tar.gz"
+    version "1.7"
+    sha256 "51d905e0b43b3d0ed41aaf23e19001ab4bc3f96c3ca134b48f7892485fc52961"
+  end
 
   def install
+    (buildpath/"gobootstrap").install resource("gobootstrap")
+    ENV["GOROOT_BOOTSTRAP"] = buildpath/"gobootstrap"
+
     cd "src" do
       ENV["GOROOT_FINAL"] = libexec
-      # Set portable defaults for CC/CXX to be used by cgo
-      with_env(CC: "cc", CXX: "c++") { system "./make.bash" }
+      ENV["GOOS"]         = "darwin"
+      system "./make.bash", "--no-clean"
     end
 
+    (buildpath/"pkg/obj").rmtree
+    rm_rf "gobootstrap" # Bootstrap not required beyond compile.
     libexec.install Dir["*"]
     bin.install_symlink Dir[libexec/"bin/go*"]
 
-    system bin/"go", "install", "std", "cmd"
+    system bin/"go", "install", "-race", "std"
 
-    # Remove useless files.
-    # Breaks patchelf because folder contains weird debug/test files
-    rm_r(libexec/"src/debug/elf/testdata")
-    # Binaries built for an incompatible architecture
-    rm_r(libexec/"src/runtime/pprof/testdata")
+    # Build and install godoc
+    ENV.prepend_path "PATH", bin
+    ENV["GO111MODULE"] = "on"
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/golang.org/x/tools").install resource("gotools")
+    cd "src/golang.org/x/tools/cmd/godoc/" do
+      system "go", "build"
+      (libexec/"bin").install "godoc"
+    end
+    bin.install_symlink libexec/"bin/godoc"
   end
 
   test do
-    (testpath/"hello.go").write <<~GO
+    (testpath/"hello.go").write <<~EOS
       package main
 
       import "fmt"
@@ -53,36 +69,17 @@ class GoAT121 < Formula
       func main() {
           fmt.Println("Hello World")
       }
-    GO
-
+    EOS
     # Run go fmt check for no errors then run the program.
     # This is a a bare minimum of go working as it uses fmt, build, and run.
     system bin/"go", "fmt", "hello.go"
     assert_equal "Hello World\n", shell_output("#{bin}/go run hello.go")
 
-    with_env(GOOS: "freebsd", GOARCH: "amd64") do
-      system bin/"go", "build", "hello.go"
-    end
+    # godoc was installed
+    assert_predicate libexec/"bin/godoc", :exist?
+    assert_predicate libexec/"bin/godoc", :executable?
 
-    (testpath/"hello_cgo.go").write <<~GO
-      package main
-
-      /*
-      #include <stdlib.h>
-      #include <stdio.h>
-      void hello() { printf("%s\\n", "Hello from cgo!"); fflush(stdout); }
-      */
-      import "C"
-
-      func main() {
-          C.hello()
-      }
-    GO
-
-    # Try running a sample using cgo without CC or CXX set to ensure that the
-    # toolchain's default choice of compilers work
-    with_env(CC: nil, CXX: nil) do
-      assert_equal "Hello from cgo!\n", shell_output("#{bin}/go run hello_cgo.go")
-    end
+    ENV["GOOS"] = "freebsd"
+    system bin/"go", "build", "hello.go"
   end
 end
